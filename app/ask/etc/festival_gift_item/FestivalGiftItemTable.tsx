f9664291704b09203ffc4b8ca1f0d9f222da1510
Lenin <41979955+liu245933567@users.noreply.github.com>
2024-08-01 17:01:06 +0800

    ✨ feat: 更新代码

--- app/ask/etc/festival_gift_item/FestivalGiftItemTable.tsx
+++ app/ask/etc/festival_gift_item/FestivalGiftItemTable.tsx
@@ -1,99 +1,134 @@
 "use client";
 
 import React from "react";
-import { Button, Space, Table, Tag } from "antd";
-
-interface DataType {
-  key: string;
-  name: string;
-  age: number;
-  address: string;
-  tags: string[];
-}
-
-const data: DataType[] = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
-    tags: ["nice", "developer"],
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    age: 42,
-    address: "London No. 1 Lake Park",
-    tags: ["loser"],
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    address: "Sydney No. 1 Lake Park",
-    tags: ["cool", "teacher"],
-  },
-];
+import { Button } from "antd";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/react";
 
 // #I宠物|冥炎之灵(鬼仙)$1$3#I#I宠物|孟姑(鬼仙)$1$3#I
 // #I消费积分|消费积分#r10000#I
 
-export const FestivalGiftItemTable: React.FC = () => (
-  <>
-    <div className="mb-4">
-      <Button type="primary">导入</Button>
-    </div>
-    <Table
-      size="small"
-      bordered
-      columns={[
-        {
-          title: "Name",
-          dataIndex: "name",
-          render: (text) => <a>{text}</a>,
-        },
-        {
-          title: "Age",
-          dataIndex: "age",
-          key: "age",
-        },
-        {
-          title: "Address",
-          dataIndex: "address",
-          key: "address",
-        },
-        {
-          title: "Tags",
-          key: "tags",
-          dataIndex: "tags",
-          render: (_, { tags }) => (
-            <>
-              {tags.map((tag) => {
-                let color = tag.length > 5 ? "geekblue" : "green";
-                if (tag === "loser") {
-                  color = "volcano";
-                }
-                return (
-                  <Tag color={color} key={tag}>
-                    {tag.toUpperCase()}
-                  </Tag>
-                );
-              })}
-            </>
-          ),
-        },
-        {
-          title: "Action",
-          key: "action",
-          render: (_, record) => (
-            <Space size="middle">
-              <a>Invite {record.name}</a>
-              <a>Delete</a>
-            </Space>
-          ),
-        },
-      ]}
-      dataSource={data}
-    />
-  </>
-);
+interface TableProps {
+  columns: { label: string; key: string }[];
+  rows: Record<string, string>[];
+}
+
+const renderResult = (result: string) => {
+  const arr = result.split("\n");
+
+  const newArr = arr.filter((i) => {
+    return i !== "" && !i.startsWith("#");
+  });
+
+  const [dataIndex, ...dataSourceStringArr] = newArr;
+
+  const dataIndexArr = dataIndex.split(",").map((i) => i.trim());
+
+  const rows: Record<string, string>[] = [];
+
+  dataSourceStringArr.forEach((i) => {
+    const arr = i.split(", ");
+    if (arr.length !== dataIndexArr.length) {
+      return;
+    }
+
+    const obj: Record<string, string> = {};
+
+    dataIndexArr.forEach((key, index) => {
+      obj[key] = arr[index].trim();
+    });
+    rows.push(obj);
+  });
+
+  const columns = dataIndexArr.map((key) => {
+    return { label: key, key };
+  });
+
+  return {
+    columns,
+    rows,
+  };
+};
+
+const UploadButton: React.FC<{
+  onChange?: (props: TableProps) => void;
+}> = ({ onChange }) => {
+  const ref = React.useRef<HTMLInputElement>(null);
+  const [files, setFiles] = React.useState<File[]>([]);
+
+  const renderFile = (file: File) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const result = e.target!.result as string;
+      if (!result) {
+        return;
+      }
+
+      onChange?.(renderResult(result));
+    };
+    reader.readAsText(file, "gb2312");
+  };
+
+  return (
+    <>
+      <input
+        ref={ref}
+        // value={files as string[]}
+        title="up"
+        className="display-none"
+        type="file"
+        onChange={(e) => {
+          const file = e.target.files![0];
+
+          if (!file) {
+            return;
+          }
+          renderFile(file);
+        }}
+      />
+      <Button type="primary">选择文件</Button>
+    </>
+  );
+};
+
+export const FestivalGiftItemTable: React.FC = () => {
+  const [tableProps, setTableProps] = React.useState<TableProps>();
+
+  return (
+    <>
+      <div className="mb-4">
+        <UploadButton onChange={setTableProps} />
+      </div>
+
+      {/* {JSON.stringify(tableProps?.rows, null, 2)} */}
+
+      {tableProps && (
+        <Table>
+          <TableHeader columns={tableProps.columns}>
+            {(column) => (
+              <TableColumn key={column.key}>{column.label}</TableColumn>
+            )}
+          </TableHeader>
+          <TableBody
+            items={tableProps.rows || ([] as Record<string, string>[])}
+          >
+            {(item) => (
+              <TableRow key={item[tableProps.columns[0].key]}>
+                {(columnKey) => (
+                  <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+                )}
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      )}
+    </>
+  );
+};
---
+++ app/ask/user-admin/page.tsx
@@ -0,0 +1,7 @@
+export default function AskUserAdminPage() {
+  return (
+    <div>
+      <h1>User Admin</h1>
+    </div>
+  );
+}
